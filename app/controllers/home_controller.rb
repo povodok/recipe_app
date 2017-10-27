08869a8ccfc67faaac78d8936c6a621ecfd1fab7
Vitalii Povod <povod.vitalii@gmail.com>
2017-10-27 13:18:49 +0300

    autocomplete in navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  autocomplete :recipe, :name, full: true
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
+  resources :home do
+    get :autocomplete_recipe_name, :on => :collection
+  end
   resources :recipes do
     get :autocomplete_ingredient_name, :on => :collection
   end
